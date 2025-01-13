86d8c463d61c3ed38462f6febba8e6860604ac16
alaminsframe <helloalaminsheikh@gmail.com>
2025-01-13 15:36:43 +0600

    factorial of a number

---
+++ algorithms/factorial-number.js
@@ -0,0 +1,30 @@
+// function factorial(n){
+//     let fac=1;
+//     for(let i=n; i>=2; i--){
+//      fac*=i;
+//     }
+//     return fac;
+// }
+// console.log(factorial(4));
+
+
+
+// function factorial(n){
+//     if (n <= 1) return 1;
+//     return n * factorial(n - 1);
+  
+// }
+
+
+function factorial(n){
+    let factor = 1;
+    for(let i=2; i<=n; i++){
+        //console.log(i);
+        
+        factor*=i;
+    }
+    return factor;
+}
+console.log(factorial(5));
+
+
