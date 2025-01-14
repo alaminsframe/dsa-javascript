0f816250eb90fa846b8d4a597d637622cb8a71c3
alaminsframe <helloalaminsheikh@gmail.com>
2025-01-14 17:19:56 +0600

    power of two in unuque way

---
+++ algorithms/big-o-notation/power-of-two.js
@@ -0,0 +1,12 @@
+function isPowerOfTwo(n){
+    let arr=[];
+    for(let i= 0; i<=Math.ceil(Math.sqrt(n)); i++){
+        if(n === 2**i) return true;
+    }
+    return false;
+}
+
+console.log(isPowerOfTwo(1));
+console.log(isPowerOfTwo(2));
+console.log(isPowerOfTwo(16));
+console.log(isPowerOfTwo(15));
