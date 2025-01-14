34ddd7bc32e7a29c83ff8120814b71d5d4a1364b
alaminsframe <helloalaminsheikh@gmail.com>
2025-01-14 15:49:20 +0600

    prime number

--- algorithms/prime-number.js
+++ algorithms/prime-number.js
@@ -4,14 +4,13 @@ function isPrime(n){
     for(let i=2; i<n; i++){
         if(n%i===0){
             return `${n} is not prime`;
-        }else{
-            return`${n} is prime`;
         }
     }
+    return `${n} is prime`;
     
 }
 
-console.log(isPrime(11));
+console.log(isPrime(35));
 
 
 // Big-O = O(n)
