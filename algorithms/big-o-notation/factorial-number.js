572e6b089ce24bde39db3507bbfe03b45d4a17a0
alaminsframe <helloalaminsheikh@gmail.com>
2025-01-16 23:53:53 +0600

    test

--- algorithms/big-o-notation/factorial-number.js
+++ algorithms/big-o-notation/factorial-number.js
@@ -9,13 +9,6 @@
 
 
 
-// function factorial(n){
-//     if (n <= 1) return 1;
-//     return n * factorial(n - 1);
-  
-// }
-
-
 function factorial(n){
     let factor = 1;
     for(let i=2; i<=n; i++){
