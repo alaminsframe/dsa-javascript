0244fa6b879d061c13da80210fc999abeac7274b
alaminsframe <helloalaminsheikh@gmail.com>
2025-01-17 01:45:36 +0600

    added time and space complexity

--- algorithms/recursion/fibonacchi-sequence.js
+++ algorithms/recursion/fibonacchi-sequence.js
@@ -2,4 +2,8 @@ function finbonacchi(n){
     if(n<2) return n;
     return finbonacchi(n-1)+finbonacchi(n-2)
 }
-console.log(finbonacchi(6));
+console.log(finbonacchi(0));
+console.log(finbonacchi(1));
+console.log(finbonacchi(7));
+
+//O(2^n) - Recursive
