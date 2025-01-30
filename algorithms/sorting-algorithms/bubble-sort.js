7491f03137e08660c26e4e75ff25e4b2e9f8afdd
alaminsframe <helloalaminsheikh@gmail.com>
2025-01-31 00:48:40 +0600

    bubble sort with for loop

--- algorithms/sorting-algorithms/bubble-sort.js
+++ algorithms/sorting-algorithms/bubble-sort.js
@@ -1,23 +1,38 @@
 //with do-while loop
+// function bubbleSort(arr) {
+//     let n = arr.length;
+    
+//     let swapped;
+//     do {
+//         swapped = false;
+//         for (let i = 0; i < n-1; i++) {
+//             if (arr[i]>arr[i+1]) {
+//                 let temp = arr[i];
+//                 arr[i] = arr[i+1];
+//                 arr[i+1]= temp;
+
+//                 swapped = true;
+//             }
+//         }
+//         n--;
+//     } while (swapped);
+
+//     return arr;
+// }
+
+
+//with for loop
 function bubbleSort(arr) {
     let n = arr.length;
-    
-    let swapped;
-    do {
-        swapped = false;
-        for (let i = 0; i < n-1; i++) {
-            if (arr[i]>arr[i+1]) {
-                let temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1]= temp;
-
-                swapped = true;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n-i-1; j++) {
+            if (arr[j]>arr[j+1]) {
+                [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
             }
         }
-        n--;
-    } while (swapped);
-
+    }
     return arr;
 }
+
 
 console.log(bubbleSort([34,42,23,46,21,53]));
