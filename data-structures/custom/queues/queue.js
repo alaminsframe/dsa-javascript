0989d0abd624fa659511ec8798218fd7e23b16d9
alaminsframe <helloalaminsheikh@gmail.com>
2025-04-21 23:32:49 +0600

    basic queue

---
+++ data-structures/custom/queues/queue.js
@@ -0,0 +1,43 @@
+class Queue {
+    constructor() {
+      this.items = [];
+    }
+  
+    // Add to the queue
+    enqueue(element) {
+      this.items.push(element);
+    }
+  
+    // Remove from the queue
+    dequeue() {
+      if (this.isEmpty()) return "Queue is empty";
+      return this.items.shift();
+    }
+  
+    // View the first item
+    front() {
+      return this.isEmpty() ? "Queue is empty" : this.items[0];
+    }
+  
+    // Check if queue is empty
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  
+    // Get size
+    size() {
+      return this.items.length;
+    }
+  
+}
+
+const q = new Queue();
+q.enqueue("Alice");
+q.enqueue("Bob");
+q.enqueue("Charlie");
+console.log(q.items);
+
+q.dequeue(); // removes Alice
+console.log(q.items);
+
+  
