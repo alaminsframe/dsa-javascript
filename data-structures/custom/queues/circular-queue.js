94da3b6ce59a39d17ff1bc1e2d5931cab93dae30
alaminsframe <helloalaminsheikh@gmail.com>
2025-05-10 18:48:12 +0600

    circular queue

---
+++ data-structures/custom/queues/circular-queue.js
@@ -0,0 +1,16 @@
+class CircularQueue{
+    constructor(capacity){
+        this.items = new Array(capacity);
+        this.capacity = capacity;
+        this.currentLength = 0;
+        this.rear = -1;
+        this.front = -1;
+    }
+
+    isFull(){
+        return this.currentLength === this.capacity
+    }
+    
+
+
+}
